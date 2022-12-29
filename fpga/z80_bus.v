809845eebbe67c5b78e8bea6190613b8da86f54d
Krišjāns Bluķis <kblukis@gmail.com>
2022-12-29 15:10:22 +0200

    Add data bus; Write to IO port 12345 will store 8-bit value in FPGA; Read from IO port 12345 will read back stored 8-bit value;

--- fpga/z80_bus.v
+++ fpga/z80_bus.v
@@ -1,14 +1,28 @@
 module z80_addr_decode (input [15:0] z80_a,
+                        inout [7:0] z80_d,
                         input z80_rd,
                         input z80_wr,
                         input z80_m1,
                         input z80_iorq,
                         input z80_mreq,
+                        output reg z80_d_oe,
+                        output reg z80_d_dir,
                         output reg test_led);
 
-    initial test_led = 1'b1;
-
     localparam Z80_IO_ADDR = 16'd12345;
+    localparam D_IN = 1'b1;
+    localparam D_OUT = 1'b0;
+
+    reg [7:0] z80_io_data;
+
+    initial begin
+        z80_d_dir <= D_IN;
+        z80_d_oe <= 1'b0;
+        z80_io_data <= 8'd0;
+        test_led <= 1'b1;
+    end
+
+    assign z80_d = z80_d_dir == D_OUT ? z80_io_data : 8'bZ;
 
     wire myIoAddr = z80_iorq == 1'b0
                     && z80_mreq == 1'b1
@@ -21,9 +35,14 @@ module z80_addr_decode (input [15:0] z80_a,
 
     always @ (negedge clk) begin
         if (z80_rd == 1'b0 && myIoAddr == 1'b1) begin
+            z80_d_dir <= D_OUT;
             test_led <= 1'b1;
         end else if (z80_wr == 1'b0 && myIoAddr == 1'b1) begin
+            z80_d_dir <= D_IN;
+            z80_io_data <= z80_d;
             test_led <= 1'b0;
+        end else begin
+            z80_d_dir <= D_IN;
         end
     end
 
